176623665cfa44d63e3f7af03318f3408421563b
Eugene Rush <rush.zlo@gmail.com>
2015-12-24 16:52:53 +0300

    we can play first note (buggy)

--- transiever/mainwindow.cpp
+++ transiever/mainwindow.cpp
@@ -1,3 +1,5 @@
+#include <QDebug>
+
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
@@ -6,9 +8,61 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    setupJackClient();
+    timer.setInterval(200);
+    timer.connect(&timer, SIGNAL(timeout()), this, SLOT(boom()));
+    timer.start();
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::boom() {
+    QtJack::MidiData note_on[]  = { 0x90, 0x1E, 0x45};
+    QtJack::MidiData note_off[] = { 0x90, 0x1E, 0x00};
+
+    _buffer.write(note_on, sizeof(note_on));
+    _buffer.write(note_off, sizeof(note_off));
+}
+
+void MainWindow::setupJackClient() {
+    // Connect to JACK server
+    _client.connectToServer("baraban");
+
+    // Create a 1000 seconds buffer (at 44,1kHz)
+    // Register two output ports
+
+    _outMidiPort     = _client.registerMidiOutPort("out");
+
+    _client.setProcessor(this);
+
+    // Activate client.
+    _client.activate();
+
+    // Connect automatically to qsynth playback.
+    _client.connect(_outMidiPort, _client.portByName("qsynth:midi"));
+}
+
+void MainWindow::process(int samples) {
+//    _buffer.copyTo(_outMidiPort.buffer(samples));
+//    _buffer.clear();
+
+//    _buffer.read()
+//    _buffer.reset();
+
+    int available = _buffer.numberOfElementsAvailableForRead() / 3;
+    QtJack::MidiBuffer out = _outMidiPort.buffer(samples);
+    int sample = 0;
+
+    while ( available && samples ) {
+        QtJack::MidiData data[3];
+        _buffer.read(data, 3);
+        out.writeEvent(sample++, data, 3);
+        available--;
+        samples--;
+    }
+
+    _buffer.reset();
 }
--- transiever/mainwindow.h
+++ transiever/mainwindow.h
@@ -2,21 +2,38 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QTimer>
+
+#include <Client>
+#include <Processor>
+#include <RingBuffer>
+#include <MidiPort>
+#include <MidiBuffer>
 
 namespace Ui {
 class MainWindow;
 }
 
-class MainWindow : public QMainWindow
+class MainWindow : public QMainWindow, public QtJack::Processor
 {
     Q_OBJECT
 
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void process(int samples);
+
+public slots:
+    void boom();
 
 private:
-    Ui::MainWindow *ui;
+    Ui::MainWindow*     ui;
+    QTimer              timer;
+    QtJack::Client      _client;
+    QtJack::MidiPort    _outMidiPort;
+    QtJack::MidiRingBuffer _buffer;
+
+    void setupJackClient();
 };
 
 #endif // MAINWINDOW_H
